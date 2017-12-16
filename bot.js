21bcb49efe41b8c21fb786bdfd0f9d5633a646b1
anequalist <34600880+anequalist@users.noreply.github.com>
2017-12-16 14:37:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on('message', message => {
+    if (message.content === '!praljak') {
+    	message.reply('https://www.jutarnji.hr/incoming/slobodan-praljak-haagjpg/6794733/alternates/LANDSCAPE_680/Slobodan%20Praljak%20Haag.jpg');
+  	}
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !praljak
-            case 'praljak':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'https://www.jutarnji.hr/incoming/slobodan-praljak-haagjpg/6794733/alternates/LANDSCAPE_680/Slobodan%20Praljak%20Haag.jpg'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
-view raw
-bot.js hosted with ❤ by GitHub
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
